4e69a96740591c7ba3eac3db6a15d61813309a9e
Henrique Rubido <henriquerubido1@gmail.com>
2022-07-28 10:16:46 -0300

    feat:selector, template and styles

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // you can use selector to select the element in the html file
+  // or you can use selector with [] to use the element in another
+  // html tag
+  // you can also use a class to select the element
+  // selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: '.app-servers',
+
   // you can use template for using html in the component
   // or you can use templateUrl to load html from a file
   template: `
     <app-server></app-server>
     <app-server></app-server>`,
   // templateUrl: './servers.component.html',
+  
   // the same thing happens for the css
   styles: [`
     h3 {
