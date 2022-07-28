0c6ff7d163cc8aefacd2c874340ab1ef28aa632d
Henrique Rubido <henriquerubido1@gmail.com>
2022-07-28 10:06:14 -0300

    feat:@Component

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,8 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  // you can use template for using html in the component
+  // or you can use templateUrl to load html from a file
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
+  // templateUrl: './servers.component.html',
+  // the same thing happens for the css
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
+  // styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
