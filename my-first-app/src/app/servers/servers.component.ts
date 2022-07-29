50d41526adcf164fbfea899a1801cec9a8310a6b
Henrique Rubido <henriquerubido1@gmail.com>
2022-07-29 10:06:58 -0300

    feat:assignment 2

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -26,8 +26,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus = 'No server was created!';
+  serverCreationStatus = 'No server was created!' ;
   serverName = 'Test server';
+  username = '';
+  buttonStatus = false;
 
   constructor() {
     setTimeout(() => {
@@ -39,11 +41,21 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
   }
 
+  onInputChange(): boolean {
+    if (this.username !== '') {
+     return this.buttonStatus = true;
+    }
+    return this.buttonStatus = false
+  }
+
+  onButtonClick() {
+    this.username = '';
+  }
 }
