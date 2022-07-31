174cc8d988912e7791528d0a80cb5be05077641a
Henrique Rubido <henriquerubido1@gmail.com>
2022-07-30 23:47:44 -0300

    feat:shopping-list components

--- course-projects/the-basics/src/app/app.module.ts
+++ course-projects/the-basics/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    ShoppingListEditComponent,
+    IngredientComponent
   ],
   imports: [
     BrowserModule
