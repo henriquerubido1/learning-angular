ebe7f7aed3d87986fadaa37f7b0fa3afac629385
Henrique Rubido <henriquerubido1@gmail.com>
2022-07-31 00:12:28 -0300

    feat:components setup

--- course-projects/the-basics/src/app/app.module.ts
+++ course-projects/the-basics/src/app/app.module.ts
@@ -4,24 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipeBookComponent,
     ShoppingListEditComponent,
-    IngredientComponent,
+    RecipeBookComponent,
+    RecipeDetailComponent,
     RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
