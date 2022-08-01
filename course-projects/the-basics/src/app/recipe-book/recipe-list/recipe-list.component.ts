67a36564c8259cb71d90aa78e9c6debb5474ce4a
Henrique Rubido <henriquerubido1@gmail.com>
2022-08-01 10:18:47 -0300

    feat:racipes list

--- course-projects/the-basics/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ course-projects/the-basics/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://static.dw.com/image/43801233_401.jpg'),
   ];
 
   constructor() { }
