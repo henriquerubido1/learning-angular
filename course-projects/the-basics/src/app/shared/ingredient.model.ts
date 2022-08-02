56820fc6de4ebbb9b9b5726c38edc8ae700d67a5
Henrique Rubido <henriquerubido1@gmail.com>
2022-08-02 09:03:21 -0300

    feat:igredient.model.ts

---
+++ course-projects/the-basics/src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient {
+  // this is a simplified way to create a new instance of an object
+  // a longer example of this section is recipe.model.ts
+  constructor(public name: string, public amount: number) {}
+}
