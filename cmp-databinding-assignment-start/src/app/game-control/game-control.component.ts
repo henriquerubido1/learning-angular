43aadb20da6d54c4c3a7898d24639487615a905c
Henrique Rubido <henriquerubido1@gmail.com>
2022-08-10 10:31:09 -0300

    feat:assignment4 part3

--- cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  timer = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onStartGame() {
+    setInterval(() => this.timer++, 1000);
+    setInterval(() => console.log(this.timer), 1000);
+  }
 }
